a68a889aedcda5a0eb1675f5ed8b1a4bd3a7a0ea
DESKTOP-16GSHC1\T <977630860@qq.com>
2018-10-30 17:08:17 +0800

    no message

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'swr_datepicker',
+    'version': '1.3',
+    'summary': 'Tools',
+    'description': """
+    
+    Odoo native date controls are extended to support year and month input and display
+==========================================
+
+    """,
+    'author': '306852160@qq.com',
+    'depends': ['web'],
+    'data': ['datepicker.xml','res.lang.csv','res_lang_view.xml'],
+    "qweb": [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
